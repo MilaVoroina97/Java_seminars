f424d71893c6001ceaa9b944d354a7db282dbe78
mila <milavoronina1997@mail.ru>
2022-12-11 01:13:25 +0500

    seminar 1

---
+++ task1.java
@@ -0,0 +1,12 @@
+package seminar;
+
+public class task1 {
+    // Дан массив чисел[1,1,0,1,1] вывести максимальное количество 1, которые идут подряд
+
+    public static void main(String[] args) {
+        
+    }
+
+
+    
+}
